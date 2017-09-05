65cc30e29802cfc1ed8246fe43a32b5f44faff59
just nobody <kingdaro@gmail.com>
2017-09-05 07:40:55 -0400

    dynamic font loading!

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
+import * as webfontloader from 'webfontloader'
 import { Game } from './game'
 import { Gameplay } from './states/gameplay'
 // import { MainMenu } from './states/main-menu'
 
-const game = new Game()
-game.setState(new Gameplay(game))
-game.run()
+function init() {
+  webfontloader.load({
+    google: {
+      families: ['Exo'],
+    },
+    active: runGame,
+  })
+}
 
-const viewRoot = document.querySelector('#game')
-if (!viewRoot) throw new Error('View root not found')
-viewRoot.appendChild(game.app.view)
+function runGame() {
+  const game = new Game()
+  game.setState(new Gameplay(game))
+  game.run()
+
+  const viewRoot = document.querySelector('#game')
+  if (!viewRoot) throw new Error('View root not found')
+  viewRoot.appendChild(game.app.view)
+}
+
+init()
